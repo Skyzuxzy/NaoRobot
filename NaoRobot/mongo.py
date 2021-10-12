a468dd1e390e5176cc87a9e57c4006ecea0c653f
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-12 14:57:47 +0700

    Update mongo.py

--- NaoRobot/mongo.py
+++ NaoRobot/mongo.py
@@ -2,10 +2,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from NaoRobot import MONGO_DB_URI 
+from LaylaRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from NaoRobot.conf import get_int_key, get_str_key
+from LaylaRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -17,8 +17,8 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["naorobot"]
+db = client["NaoRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
- except ServerSelectionTimeoutError:
+except ServerSelectionTimeoutError:
     sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
