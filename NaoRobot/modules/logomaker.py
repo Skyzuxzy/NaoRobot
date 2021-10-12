da143ae212f18b695dac9f4ae3b7b825e57f66cf
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2021-10-12 22:20:37 +0700

    Update logomaker.py

--- NaoRobot/modules/logomaker.py
+++ NaoRobot/modules/logomaker.py
@@ -40,7 +40,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
+   await event.reply(f'Error Report @kenbotsupport, {e}')
 
 
 
@@ -80,7 +80,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
+   await event.reply(f'Error Report @kenbotsupport, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
