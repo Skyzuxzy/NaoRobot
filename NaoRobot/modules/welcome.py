ff24099d61039fd8315e4fcf2f5a987aef4f1c34
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-13 21:37:23 +0700

    ...

--- NaoRobot/modules/welcome.py
+++ NaoRobot/modules/welcome.py
@@ -1144,7 +1144,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "Arrivals"
+__mod_name__ = "Welcomes"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
