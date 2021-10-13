411adeb89361ef84dd753e37eb8010e2f46d1a4f
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-13 21:40:16 +0700

    Update games.py

--- NaoRobot/modules/games.py
+++ NaoRobot/modules/games.py
@@ -65,12 +65,14 @@ async def _(event):
 
 
 __help__ = """
- *Play Game With Emojis:*
+ *This is fun commands:*
   - /dice or /dice 1 to 6 any value
   - /ball or /ball 1 to 5 any value
   - /dart or /dart 1 to 6 any value
+  - /couples - Get couple of today
+  - /tagall - Tag everyone on your groups
  Usage: hahaha just a magic.
  warning: you would be in trouble if you input any other value than mentioned.
 """
 
-__mod_name__ = "Game"
+__mod_name__ = "Fun&Game"
