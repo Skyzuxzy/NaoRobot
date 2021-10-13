65538bd35fe35fe128780aca99960a437ea0fca4
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-13 12:09:40 +0700

    Update heroku.py

--- NaoRobot/modules/heroku.py
+++ NaoRobot/modules/heroku.py
@@ -191,7 +191,7 @@ async def _(dyno):
         dyno.chat_id,
         "logs.txt",
         reply_to=dyno.id,
-        caption="yone Bot Logz.",
+        caption="NaoRobot.",
     )
 
     await asyncio.sleep(5)
