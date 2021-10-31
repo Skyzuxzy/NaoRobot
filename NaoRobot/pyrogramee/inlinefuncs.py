9a024466f52dcb1e05cd5e4b6e0e6277fe631f5f
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-10-31 16:24:11 +0700

    Update inlinefuncs.py

--- NaoRobot/pyrogramee/inlinefuncs.py
+++ NaoRobot/pyrogramee/inlinefuncs.py
@@ -29,7 +29,7 @@ from NaoRobot.utils.keyboard import ikb
 from NaoRobot.pyrogramee.tasks import _get_tasks_text, all_tasks, rm_task
 from NaoRobot.pyrogramee.types import InlineQueryResultCachedDocument
 from NaoRobot.modules.info import get_chat_info, get_user_info
-from NaoRobot.modules.media import download_youtube_audio
+from NaoRobot.modules.music import download_youtube_audio
 from NaoRobot.utils.functions import test_speedtest
 from NaoRobot.utils.pastebin import paste
 
