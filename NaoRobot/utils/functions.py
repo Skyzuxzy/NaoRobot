bc6b6a2c704db07ed160f942b21b0d31268c0cbf
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-11-01 11:23:46 +0700

    Update functions.py

--- NaoRobot/utils/functions.py
+++ NaoRobot/utils/functions.py
@@ -22,51 +22,6 @@ async def restart(m: Message):
     execvp(executable, [executable, "-m", "NaoRobot"])
 
 
-def generate_captcha():
-    # Generate one letter
-    def gen_letter():
-        return chr(randint(65, 90))
-
-    def rndColor():
-        return (randint(64, 255), randint(64, 255), randint(64, 255))
-
-    def rndColor2():
-        return (randint(32, 127), randint(32, 127), randint(32, 127))
-
-    # Generate a 4 letter word
-    def gen_wrong_answer():
-        word = ""
-        for _ in range(4):
-            word += gen_letter()
-        return word
-
-    # Generate 8 wrong captcha answers
-    wrong_answers = []
-    for _ in range(8):
-        wrong_answers.append(gen_wrong_answer())
-
-    width = 80 * 4
-    height = 100
-    correct_answer = ""
-    font = ImageFont.truetype("assets/arial.ttf", 55)
-    file = f"assets/{randint(1000, 9999)}.jpg"
-    image = Image.new("RGB", (width, height), (255, 255, 255))
-    draw = ImageDraw.Draw(image)
-
-    # Draw random points on image
-    for x in range(width):
-        for y in range(height):
-            draw.point((x, y), fill=rndColor())
-
-    for t in range(4):
-        letter = gen_letter()
-        correct_answer += letter
-        draw.text((60 * t + 50, 15), letter, font=font, fill=rndColor2())
-    image = image.filter(ImageFilter.BLUR)
-    image.save(file, "jpeg")
-    return [file, correct_answer, wrong_answers]
-
-
 def test_speedtest():
     def speed_convert(size):
         power = 2 ** 10
